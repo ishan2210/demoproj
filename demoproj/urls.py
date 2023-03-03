5d371cc0a44d2163f1747c1f349c5e431860d70b
ishan2210 <ishankansara10@gmail.com>
2023-03-03 12:20:34 +0530

    commit

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def projects(request,pk):
-    return HttpResponse("Here are my projects"+ " "+str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/<str:pk>/',projects,name="projects"),
+    path('',include('projects.urls'))
+    
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request,pk):
+    return HttpResponse('Here are our projects'+ " "+ str(pk))
+def project(request,pk):
+    return HttpResponse("Single Project"+ " "+str(pk))
