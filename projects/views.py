0ba3a3bfd8d4fa09af0e9e1cc3ee04e71f3e0188
ishan2210 <ishankansara10@gmail.com>
2023-03-03 12:42:33 +0530

    commit

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request,pk):
-    return HttpResponse('Here are our projects'+ " "+ str(pk))
+    return render(request,'projects.html')
+    #return HttpResponse('Here are our projects'+ " "+ str(pk))
 def project(request,pk):
-    return HttpResponse("Single Project"+ " "+str(pk))
+    return render(request,"single-project.html")
+    #return HttpResponse("Single Project"+ " "+str(pk))
