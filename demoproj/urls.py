93bac067ad250e940fdf78684e2965f88163cacf
ishan2210 <ishankansara10@gmail.com>
2023-03-03 11:36:31 +0530

    commit

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -2,20 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def projects(request):
-    return HttpResponse("Here are our projects")
-
-def ishan(request):
-    return HttpResponse("Ishan Kansara King of mirzapur")
-
-def demo(request,pk):
-    return HttpResponse("Demo project"+ " "+ str(pk))
-
-
+def projects(request,pk):
+    return HttpResponse("Here are my projects"+ " "+str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/',projects,name="projects"),
-    path('ishan/',ishan,name="ishan"),
-    path('demo/<str:pk>/',demo,name="demo"),
+    path('projects/<str:pk>/',projects,name="projects"),
 ]
