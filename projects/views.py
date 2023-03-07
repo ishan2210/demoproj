c63dccdc82b811903d5195f85914b543a121be27
ishan2210 <ishankansara10@gmail.com>
2023-03-07 13:36:42 +0530

    commit

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+
+    {
+      'id': '1',
+      'title': 'Ecommerce Website',
+      'description': 'Fully functional ecommerce website'
+    },
+    {
+       'id': '2',
+       'title': 'Portfolio Website',
+       'description': 'This was a project where we built our portfolio'
+    },
+    {
+       'id': '3',
+       'title': 'Socialmedia website',
+       'description': 'This is our social media page'
+    },
+
+]
+
 def projects(request):
     page = 'projects'
-    return render(request,'projects.html', {'page': page})
+    number=10
+    context={'page': page,'number':number,'projects':projectsList}
+    return render(request,'projects.html',context)
     #return HttpResponse('Here are our projects')
 def project(request,pk):
     return render(request,"single-project.html")
